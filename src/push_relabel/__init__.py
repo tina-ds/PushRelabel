9467a5da63e12b320ba539efef1a7d7b1cc022a1
tina-ds <69384619+tina-ds@users.noreply.github.com>
2026-07-15 19:08:13 +0300

    Export highest-label implementation

--- src/push_relabel/__init__.py
+++ src/push_relabel/__init__.py
@@ -1,5 +1,6 @@
 """Push-Relabel implementations extracted from the research notebook."""
 
 from .base import Edge, PushRelabel, compute_max_flow
+from .highest import PushRelabelHighest
 
-__all__ = ["Edge", "PushRelabel", "compute_max_flow"]
+__all__ = ["Edge", "PushRelabel", "PushRelabelHighest", "compute_max_flow"]
